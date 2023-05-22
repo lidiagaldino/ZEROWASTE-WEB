23aa95bf025f54491b4d91f01e997e2ff0e92335
EDUARDO PERUCCI MARTINS DE SOUZA <22102325@dsn127m1246730.cfp127aluno.local>
2023-05-22 11:19:25 -0300

    a

--- src/views/pontuation/components/PontuationCupom.tsx
+++ src/views/pontuation/components/PontuationCupom.tsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import '../styles/pontuation.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import { faXmark, faGift } from '@fortawesome/free-solid-svg-icons'
+import 'animate.css';
 import Swal from 'sweetalert2';
+import { Translate } from 'phosphor-react';
 
 const PontuationCupom = () => {
 
@@ -70,10 +72,11 @@ const PontuationCupom = () => {
 	} else {
 		document.body.classList.remove('active-modal')
 	}
-
+	
 	//
 
 	const [notEnought, setNotEnought] = useState(false)
+	const [enought, setEnought] = useState(false)
 
 	const reedemCupom = async (e) => {
 		const reedem = await fetch(`https://zero-waste-logistic.azurewebsites.net/coupon/${e.currentTarget.id}`, {
@@ -83,37 +86,64 @@ const PontuationCupom = () => {
 			}
 		})
 
-		if(reedem.ok){
+		if (reedem.ok) {
+			setEnought(true)
 			openModalCupom()
-		} else{
+		} else {
 			setNotEnought(true)
 		}
 
 	}
+
+	//
+
+	const [uniqueCupom, setUniqueCupom] = useState(0)
+
+	const uniqueCupomCod = async (e) => {
+		await fetch(`https://zero-waste-logistic.azurewebsites.net/coupon/unique/${e.currentTarget.id}`, {
+			method: 'GET',
+			headers: {
+				'content-type': 'application/json', 'Authorization': 'Bearer' + ' ' + localStorage.getItem('token')
+			}
+		}).then(res => res.json()).then(resposta => setUniqueCupom(resposta.codigo))
+	}
+
+	const [click, setClick] = useState(false)
+	const [a, setA]= useState(false)
+
+	//
+
+	useEffect(() => {
+		if (notEnought) {
+		  const timeout = setTimeout(() => {
+			setNotEnought(false)
+			setA(true)
+		  }, 5000);
+	
+		  return () => {
+			clearTimeout(timeout);
+			setA(false)
+		  };
+		}
+	  }, [notEnought]);
 
 
 	return (
 		<div className="containerPontuation">
 			<div className="saldo-container">
 				<div className="saldo">
-					<>
-					{notEnought && (	
-						 <div className="containerNotEnought">
-						 <div className="iconNotEnought">
-						   <div className="circleNotEnought">
-							 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">--&gt;<path d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z" /></svg>
-						   </div>
-						 </div>
-						 <div className="infoNotEnought">
-						   <h1>Atencao</h1>
-						   <span>Voce nao tem pontos suficientes <div className="X"><FontAwesomeIcon icon={faXmark} className="icon-xmark" /></div></span>
-						 </div>
-					   </div>
-					)}
-					</>
 					<h1>Saldo: <h1 className='SaldoTitle' style={{ paddingRight: 10 }}>{dataCupom}</h1>pontos</h1>
 					<span>Ganhe pontos para trocar com recompensas exclusivas com nossos parceiros, acumule pontos fazendo reciclagem ou coletando reciclagem</span>
 				</div>
+				<>
+					{notEnought && (
+							<>
+								<div className="notEnought animate__animated animate__fadeInLeft">
+									<span>Você não tem pontos suficientes</span>
+								</div>
+							</>
+					)}
+				</>
 			</div>
 			<div className="box-cupom">
 				<h2 style={{ paddingLeft: 30, paddingTop: 20, fontSize: 20 }}>Disponiveis:</h2>
@@ -133,10 +163,8 @@ const PontuationCupom = () => {
 											</div>
 											<div className="couponItem-action">
 												<button id={elemento.id} key={elemento.id} onClick={(e) => {
-												
-												
 													reedemCupom(e)
-													
+													uniqueCupomCod(e)
 												}}>{elemento.pontos} PONTOS</button>
 											</div>
 										</li>
@@ -146,14 +174,17 @@ const PontuationCupom = () => {
 						})}
 
 						{modal && (
-							 <div className="modal-aval ">
-							 <div className="overlay-aval "></div>
-								<div className="containerCupom">
+							<div className="modal-aval ">
+								<div className="overlay-aval "></div>
+								<div className="containerCupom">									
 									<div className="containerCupom2">
-									<FontAwesomeIcon icon={faXmark} onClick={openModalCupom} className="" />
+										{/*<FontAwesomeIcon icon={faGift} className="giftCupom" style={{color: "#ffffff",}} />*/}
 										<div className="titleReedem">
-											<h1>CUPOM RESGATADO COM SUCESSO</h1>
+	
+											<h1>{click ? 'CUPOM RESGATADO COM SUCESSO' : 'RESGATAR ESTE CUPOM ?'}</h1>
+											<FontAwesomeIcon onClick={() => { openModalCupom() }} icon={faXmark} className="Xmarkk" style={{ color: "#000", paddingLeft: 20 }} />
 										</div>
+										
 										<div className="descCupom">
 											<span>* 20% OFF em todas as compras do supermercado Giga!</span>
 										</div>
@@ -161,7 +192,15 @@ const PontuationCupom = () => {
 											<span>Disponivel apenas em redes Giga</span>
 											<span>Disponivel apenas em redes Giga</span>
 										</div>
+										<div  onClick={(e) => { 
+											setClick(true)
+											
+										}} style={{ flexDirection: 'column', cursor: 'pointer' }} className="codigo">
+											<span>Codigo:</span>
+											<h1>{click ? uniqueCupom : 'RESGATAR'}</h1>
+										</div>
 									</div>
+
 								</div>
 							</div>
 
@@ -170,29 +209,31 @@ const PontuationCupom = () => {
 
 
 
+
+
 					</div>
 					<h2 style={{ paddingLeft: 20, paddingTop: 20, fontSize: 20 }}>Resgatados:</h2>
 					<div className="cumpon-resgatados">
-					{alreadyReedem.map((elemento) => {
+						{alreadyReedem.map((elemento) => {
 							return (
 								<>
-						<ul className="couponList">
-							<li className="couponItem">
-								<div className="couponItem-info">
-									<h1 className="couponItem-title">{elemento.nome}</h1>
-									<a className="couponItem-link" href="#" title="Cupons de desconto do Submarino">
-										<img src="https://cuponomia-a.akamaihd.net/img/stores/small/submarino.png" alt="Logo do Submarino" />
-									</a>
-									<span className="couponItem-desc"><strong>Regras: </strong>{elemento.criterios}</span>
-								</div>
-								<div className="couponItem-action">
-								<button>Resgatado</button>
-								</div>
-							</li>
-						</ul>
-						</>
-						)
-					})}
+									<ul className="couponList">
+										<li className="couponItem">
+											<div className="couponItem-info">
+												<h1 className="couponItem-title">{elemento.nome}</h1>
+												<a className="couponItem-link" href="#" title="Cupons de desconto do Submarino">
+													<img src="https://cuponomia-a.akamaihd.net/img/stores/small/submarino.png" alt="Logo do Submarino" />
+												</a>
+												<span className="couponItem-desc"><strong>Regras: </strong>{elemento.criterios}</span>
+											</div>
+											<div className="couponItem-action">
+												<button>Resgatado</button>
+											</div>
+										</li>
+									</ul>
+								</>
+							)
+						})}
 					</div>
 				</div>
 			</div>
